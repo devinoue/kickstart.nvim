16c3d2b3fc85fc7fd63351291c79c60d8fc94a31
Devinoue <itisagoodcat@gmail.com>
2024-09-12 00:38:17 +0900

    fix: 注釈を追加

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -1,36 +1,36 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
+  'goolord/alpha-nvim', -- プラグインの名前
+  event = 'VimEnter', -- Vimが起動したときにプラグインを読み込むイベント
   config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+    local alpha = require 'alpha' -- alphaモジュールを読み込む
+    local dashboard = require 'alpha.themes.dashboard' -- alphaのダッシュボードテーマを読み込む
 
-    -- Set header
+    -- ヘッダーを設定
     dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
     }
 
-    -- Set menu
+    -- メニューを設定
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button('e', '  > New File', '<cmd>ene<CR>'), -- 新しいファイルを作成
+      dashboard.button('SPC ee', '  > Toggle file explorer', '<cmd>NvimTreeToggle<CR>'), -- ファイルエクスプローラーを切り替え
+      dashboard.button('SPC ff', '󰱼 > Find File', '<cmd>Telescope find_files<CR>'), -- ファイルを検索
+      dashboard.button('SPC fs', '  > Find Word', '<cmd>Telescope live_grep<CR>'), -- 単語を検索
+      dashboard.button('SPC wr', '󰁯  > Restore Session For Current Directory', '<cmd>SessionRestore<CR>'), -- 現在のディレクトリのセッションを復元
+      dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'), -- NVIMを終了
     }
 
-    -- Send config to alpha
+    -- 設定をalphaに送信
     alpha.setup(dashboard.opts)
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    -- alphaバッファでの折りたたみを無効にする
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
   end,
 }
