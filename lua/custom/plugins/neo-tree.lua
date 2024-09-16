dc0a126886b77bfd834cbeb562b6eabb88fefa45
Devinoue <itisagoodcat@gmail.com>
2024-09-17 01:49:05 +0900

    fix: 隠れファイルをでるよう修正

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -2,38 +2,33 @@ return {
   'nvim-neo-tree/neo-tree.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  opts = {
-    filesystem = {
-      follow_current_file = { enabled = true },
-      hijack_netrw_behavior = 'open_current',
-      use_libuv_file_watcher = true,
-      filtered_items = {
-        visible = true, -- デフォルトで隠されているかどうか
-        show_hidden_count = true,
-        hide_dotfiles = false, -- dotfileを隠すかどうか
-        hide_gitignored = false, -- gitignoreされているファイルを隠すかどうか
-        hide_by_name = {
-          'node_modules',
-          'thumbs.db',
-        },
-        never_show = {
-          '.git',
-          '.DS_Store',
-          '.history',
-        },
-      },
-    },
   },
   config = function()
     require('neo-tree').setup {
-      -- Neo-treeの設定をここに追加
+      filesystem = {
+        follow_current_file = { enabled = true },
+        hijack_netrw_behavior = 'open_current',
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false, -- ドットファイルを表示
+          hide_gitignored = false,
+          hide_by_name = {
+            'node_modules',
+            'thumbs.db',
+          },
+          never_show = {
+            '.git',
+            '.DS_Store',
+            '.history',
+          },
+        },
+      },
     }
-
-    -- キーマップを設定
+    -- キーマップ設定
     vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
   end,
 }
